49de5b0e049343206f1122675d92f8915dc102b5
assane-mbodj <73284269+assane-mbodj@users.noreply.github.com>
2022-11-06 14:26:00 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#smoove
+
+wget https://github.com/brentp/smoove/releases/download/v0.2.8/smoove
+chmod 755 smoove
+mv smoove ~/anaconda3/envs/dupfinder_env/bin/
+
+#Duphold
+wget https://github.com/brentp/duphold/releases/download/v0.2.3/duphold
+
+chmod 755 duphold && \
+mv duphold ~/anaconda3/envs/dupfinder_env/bin/
+
