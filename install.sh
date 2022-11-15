173d157823c494f999deb4c7e45e7c96249f0058
mbodja <73284269+assane-mbodj@users.noreply.github.com>
2022-11-15 17:28:31 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-#smoove
+#lumpy-sv
+sudo apt install lumpy-sv && \
 
-wget https://github.com/brentp/smoove/releases/download/v0.2.8/smoove
+#smoove
+wget https://github.com/brentp/smoove/releases/download/v0.2.8/smoove && \
 chmod 755 smoove
-mv smoove ~/anaconda3/envs/dupfinder_env/bin/
+mv smoove ~/anaconda3/envs/dupfinder_env/bin/ && \
 
 #Duphold
-wget https://github.com/brentp/duphold/releases/download/v0.2.3/duphold
+wget https://github.com/brentp/duphold/releases/download/v0.2.3/duphold 
 
 chmod 755 duphold && \
 mv duphold ~/anaconda3/envs/dupfinder_env/bin/
-
