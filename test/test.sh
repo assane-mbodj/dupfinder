5633b5b0f72a26aff31175cac38c4fb2b11e8999
mbodja <73284269+assane-mbodj@users.noreply.github.com>
2022-11-18 12:40:49 +0100

    Update and rename test to test.sh

---
+++ test/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e 
+
+mkdir -p test_wd && cd test_wd && \
+
+nextflow run ../dupfinder/dupfinder.nf \
+--c ../dupfinder/nextflow.config \
+--genome_file ../test_data/Arabido.fasta \
+--reads "../test_data/sample_{R1,R2}.fastq.gz" \
+--annot ../test_data/Araport11_gene.gff.gz \
+--out test_out \
+-w test_work
+
+echo "test successful: Goog Analyze with dupfinder pipeline"
