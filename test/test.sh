8dacecb90543ea4cef431ea8ef85ffb5454cba72
mbodja <73284269+assane-mbodj@users.noreply.github.com>
2022-11-18 14:02:12 +0100

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -5,9 +5,9 @@ mkdir -p test_sample && cd test_sample && \
 
 nextflow run ../dupfinder/dupfinder.nf \
 --c ../dupfinder/nextflow.config \
---genome_file ../test_data/Arabido.fasta \
---reads "../test_data/sample_{R1,R2}.fastq.gz" \
---annot ../test_data/Araport11_gene.gff.gz \
+--genome_file ../test/Arabido.fasta \
+--reads "../test/sample_{R1,R2}.fastq.gz" \
+--annot ../test/Araport11_gene.gff.gz \
 --out test_out \
 -w test_work
 
